49dbda5ae4a5637863a163586aaa20d78c31209f
Nico <ainililia@163.com>
2021-11-10 20:25:29 +0800

    init

---
+++ src/main/java/com/isnico/api/controller/MailController.java
@@ -0,0 +1,62 @@
+package com.isnico.api.controller;
+
+import java.util.concurrent.TimeUnit;
+
+import javax.validation.constraints.Pattern;
+
+import com.isnico.api.consts.CacheConst;
+import com.isnico.api.consts.SystemConst;
+import com.isnico.api.domain.vo.ResponseCode;
+import com.isnico.api.domain.vo.ResponseVo;
+import com.isnico.api.utils.SmsUtils;
+import com.isnico.api.component.CacheComponent;
+import com.isnico.api.component.MailComponent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
+@RestController
+@RequestMapping("/main")
+@Validated
+public class MailController {
+
+	@Autowired
+	private MailComponent mailComponent;
+	
+	@Autowired
+	private CacheComponent cacheComponent;
+	
+	@ApiOperation(value = "发送业务邮件")
+	@PostMapping("/business/send")
+	public ResponseVo<String> send(
+			@ApiParam(value = "账户", required = true) @Pattern(regexp = "^\\w+((-\\w+)|(\\.\\w+))*\\@[A-Za-z0-9]+((\\.|-)[A-Za-z0-9]+)*\\.[A-Za-z0-9]+$", message = "不符合邮箱格式") @RequestParam String account,
+			@ApiParam(value = "类型，1：注册，2：登录", required = true) @RequestParam int type
+			){
+		ResponseCode code = null;
+		String authCode = SmsUtils.createCode(SystemConst.MAIL_AUTH_CODE_LENGTH);
+		boolean success = mailComponent.sendSimpleMail(account, "Trap Register Auth Code Mail !", "Auth Code: " + authCode);
+		if(success) {
+			if(type == 1) {
+				cacheComponent.setEx(CacheConst.TRAP_USER_REGISTER_AUTH_CODE + account, authCode, 3L, TimeUnit.MINUTES);
+			}else {
+				cacheComponent.setEx(CacheConst.TRAP_USER_LOGIN_AUTH_CODE + account, authCode, 3L, TimeUnit.MINUTES);
+			}
+			
+			code = ResponseCode.SUCCESS;
+		}else {
+			code = ResponseCode.ERROR;
+		}
+		return new ResponseVo<>(code);
+	}
+	
+	
+	
+	
+	
+}
---
+++ src/main/java/com/isnico/api/service/UserService.java
@@ -0,0 +1,95 @@
+package com.isnico.api.service;
+
+import java.util.List;
+
+import com.isnico.api.domain.po.User;
+import com.isnico.api.domain.vo.user.UserRestVo;
+import com.isnico.api.mapper.UserMapper;
+import org.apache.ibatis.annotations.Param;
+import org.nico.ourbatis.entity.Page;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService{
+	
+	@Autowired
+	private UserMapper userMapper;
+	
+	public User selectById(String key) {
+		return userMapper.selectById(key);
+	}
+
+	public User selectEntity(User condition) {
+		return userMapper.selectEntity(condition);
+	}
+
+	public List<User> selectList(User condition) {
+		return userMapper.selectList(condition);
+	}
+
+	public long selectCount(Object condition) {
+		return userMapper.selectCount(condition);
+	}
+
+	public List<User> selectPage(Page<Object> page) {
+		return userMapper.selectPage(page);
+	}
+
+	public String selectId(User condition) {
+		return userMapper.selectId(condition);
+	}
+
+	public List<String> selectIds(User condition) {
+		return userMapper.selectIds(condition);
+	}
+
+	public int insert(User entity) {
+		return userMapper.insert(entity);
+	}
+
+	public int insertSelective(User entity) {
+		return userMapper.insertSelective(entity);
+	}
+
+	public int insertBatch(List<User> list) {
+		return userMapper.insertBatch(list);
+	}
+
+	public int update(User entity) {
+		return userMapper.update(entity);
+	}
+
+	public int updateSelective(User entity) {
+		return userMapper.updateSelective(entity);
+	}
+
+	public int updateBatch(List<User> list) {
+		return userMapper.updateBatch(list);
+	}
+
+	public int delete(User condition) {
+		return userMapper.delete(condition);
+	}
+
+	public int deleteById(String key) {
+		return userMapper.deleteById(key);
+	}
+
+	public int deleteBatch(List<String> list) {
+		return userMapper.deleteBatch(list);
+	}
+
+	/**
+	 * 通过userId查询用户简单信息
+	 * 	- 头像
+	 * 	- 昵称
+	 * 
+	 * @param userId 用户Id
+	 * @return {@link UserRestVo}
+	 */
+	public UserRestVo selectRestUseInfo(String userId) {
+		return userMapper.selectRestUseInfo(userId);
+	}
+	
+}
